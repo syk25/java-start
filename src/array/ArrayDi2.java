3eebfe3645ac9e8b69c1d1420e5790dcf370d8a8
syk25 <syk2245@gmail.com>
2023-12-25 16:26:58 +0900

    study: 2차원 배열

---
+++ src/array/ArrayDi2.java
@@ -0,0 +1,17 @@
+package array;
+
+public class ArrayDi2 {
+    public static void main(String[] args) {
+        int[][] arr = new int[][]{
+                {1,2,3},
+                {4,5,6}
+        };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.printf("arr[%d][%d]: %d \n",i,j,arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
